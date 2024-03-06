b264079ca936f1154d80671e51cb7c1b66c85799
Oscar <zipper91191@gmail.com>
2024-03-05 20:07:17 -0600

    deploy

--- src/config/db.js
+++ src/config/db.js
@@ -5,20 +5,25 @@ dotenv.config({
     path: '.env'
 });
 //Datos de conexion a la base de datos
-const dataBase = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWD ?? '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: '3306',
-    define: {
-        timestamps: false
-    }, pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    operatorAliases: false,
+const dataBase = new Sequelize(
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_USERNAME,
+    process.env.DATABASE_PASSWORD,
+    {
+        host: process.env.DATABASE_HOST,
+        dialect: 'mysql',
+        port: process.env.DATABASE_PORT,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+        define: {
+            timestamps: false,
 
-});
+        }
+    }
+);
 
 export default dataBase;
