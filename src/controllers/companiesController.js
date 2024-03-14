2e2e2e2df24d34777839154fe45d807f5ba2f45c
Watchvader <martinez.diego.1am@gmail.com>
2024-03-14 17:50:50 -0600

    se creo el controller de companies con los nuevos campos

--- src/controllers/companiesController.js
+++ src/controllers/companiesController.js
@@ -1,58 +1,68 @@
 import companiesSchema from '../models/Companies.js';
 
 const createCompany = async (req, res) => {
-    const { name, description, logo } = req.body;
+    const { companyName, companyCountry, productType, companyPhone, companyContact, companyRfc } = req.body;
     try {
-        const company = new companiesSchema({ name, description, logo });
-        await company.save();
-        res.status(200).json({ status: 200, msg: 'Empresa creada correctamente' });
+        const newCompany = await companiesSchema.create({ 
+            companyName, companyCountry, productType, companyPhone, companyContact, companyRfc 
+        });
+        
+        res.status(200).json({ status: 200, msg: 'Empresa creada correctamente', company:newCompany });
     } catch (error) {
-        res.status(500).json({ status: 500, msg: error });
+        res.status(500).json({ status: 500, msg: 'Error al crear empresa', error:error.message });
     }
 }
 
 const getAllCompanies = async (req, res) => {
     try {
         const companies = await companiesSchema.findAll();
-        res.status(200).json({ status: 200, companies });
+        res.status(200).json({ status: 200, msg: 'Empresas encontradas', companies });
     } catch (error) {
-        res.status(500).json({ status: 500, msg: error });
+        res.status(500).json({ status: 500, msg: 'Error al encontrar empresas', error:error.message  });
     }
-}
+} 
 
 const getCompanyById = async (req, res) => {
-    const { id } = req.params;
+    const companyId = req.params.id;
     try {
-        const company = await companiesSchema.findOne({ where: { id } });
-        res.status(200).json({ status: 200, company });
+        const company = await companiesSchema.findByPk(companyId);
+        if (!company) {
+            return res.status(404).json({ status: 404, msg: 'empersa no encontrada' });
+        }
+        res.status(200).json({ status: 200, msg:'empresa encontrada', company });
     } catch (error) {
-        res.status(500).json({ status: 500, msg: error });
+        res.status(500).json({ status: 500, msg:'error al encontrar la empresa', error:error.message});
     }
 }
 
 const updateCompany = async (req, res) => {
-    const { id } = req.params;
-    const { name, description, logo } = req.body;
+    const companyId = req.params.id;
+    const { companyName, companyCountry, productType, companyPhone, companyContact, companyRfc } = req.body;
     try {
-        const company = await companiesSchema.findOne({ where: { id } });
-        company.name = name;
-        company.description = description;
-        company.logo = logo;
-        await company.save();
-        res.status(200).json({ status: 200, msg: 'Empresa actualizada correctamente' });
+        const company = await companiesSchema.findByPk(companyId);
+        if (!company) {
+            return res.status(404).json({ status: 404, msg: 'empersa no encontrada' });
+        }
+        await company.update({
+            companyName, companyCountry, productType, companyPhone, companyContact, companyRfc
+        });
+        res.status(200).json({ status: 200, msg: 'Empresa actualizada correctamente', company });
     } catch (error) {
-        res.status(500).json({ status: 500, msg: error });
+        res.status(500).json({ status: 500, msg: 'error al actualizar empresa', error:error.message});
     }
 }
 
 const deleteCompany = async (req, res) => {
-    const { id } = req.params;
+    const companyId = req.params.id;
     try {
-        const company = await companiesSchema.findOne({ where: { id } });
-        await company.destroy();
+        const company = await companiesSchema.findByPk(companyId);
+        if (!company) {
+            return res.status(404).json({ status: 404, msg: 'empersa no encontrada' });
+        }
+        await companiesSchema.destroy({where: {id:companyId}});
         res.status(200).json({ status: 200, msg: 'Empresa eliminada correctamente' });
     } catch (error) {
-        res.status(500).json({ status: 500, msg: error });
+        res.status(500).json({ status: 500, msg: 'Error al eliminar empresa', error: error.message });
     }
 }
 
