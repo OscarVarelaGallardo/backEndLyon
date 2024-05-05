2ee4338b783f85cbe0e4cdcedf0573e3f3091c70
Oscar <edevelop@e-bitware.com.mx>
2024-05-05 15:23:52 -0600

    change get to post

--- src/controllers/shoppingCarController.js
+++ src/controllers/shoppingCarController.js
@@ -16,9 +16,7 @@ const createShoppingCart = async (req, res) => {
         //reestar quantity
         const product = await Products.findById(productId);
         //validat stock
-        if (product.stock <= quantity) {
-            return res.status(400).json({ status: 400, msg: 'No hay suficiente stock' });
-        }
+       
 
         if (!product) {
             return res.status(404).json({ status: 404, msg: 'Producto no encontrado' });
